b7539704e160b5ecee55cdffa6d4ec617d614944
aytch4 <73347541+aytch4@users.noreply.github.com>
2020-11-19 11:16:31 -0500

    Add files via upload

--- Wk3HW_part2.js
+++ Wk3HW_part2.js
@@ -1,5 +1,6 @@
 
-//7.	Write a function that takes two parameters, word and n, as arguments and returns the word concatenated to itself n number of times. (i.e. if I pass in ‘Hello’ and 3, I would expect the function to return ‘HelloHelloHello’).
+//7.	Write a function that takes two parameters, word and n, as arguments 
+//and returns the word concatenated to itself n number of times. (i.e. if I pass in ‘Hello’ and 3, I would expect the function to return ‘HelloHelloHello’).
 console.log("7.")
 
 function getRepeatedWord(word, n) {
@@ -14,7 +15,8 @@ function getRepeatedWord(word, n) {
 console.log(getRepeatedWord("Hello", 3))
 
 
-//8.	Write a function that takes two parameters, firstName and lastName, and returns a full name (the full name should be the first and the last name separated by a space).
+//8.	Write a function that takes two parameters, firstName and lastName,
+// and returns a full name (the full name should be the first and the last name separated by a space).
 console.log("8.");
 function getFullName(firstName, lastName) {
     return firstName + " " + lastName;
@@ -23,7 +25,8 @@ function getFullName(firstName, lastName) {
 console.log(getFullName("Joe", "Schmoe"));
 
 
-//9.	Write a function that takes an array of numbers and returns true if the sum of all the numbers in the array is greater than 100.
+//9.	Write a function that takes an array of numbers and returns true 
+//if the sum of all the numbers in the array is greater than 100.
 
 console.log("9.");
 
@@ -43,7 +46,8 @@ function greaterThanAHundred() {
 greaterThanAHundred();
 
 
-//10.	Write a function that takes an array of numbers and returns the average of all the elements in the array.
+//10.	Write a function that takes an array of numbers
+// and returns the average of all the elements in the array.
 console.log("10.");
 
 var numbers = [3, 54, 21]
@@ -59,7 +63,8 @@ function getAverage() {
 getAverage();
 
 
-//11.	Write a function that takes two arrays of numbers and returns true if the average of the elements in the first array is greater than the average of the elements in the second array.
+//11.	Write a function that takes two arrays of numbers and returns true 
+//if the average of the elements in the first array is greater than the average of the elements in the second array.
 console.log("11.")
 //use function above to calculate average for arrays
 var numbers1 = [4, 7, 2, 2]
@@ -83,7 +88,8 @@ console.log(avNumber1 > avNumber2)
 compareAverages();
 
 
-//Create a function called willBuyDrink that takes a boolean isHotOutside, and a number moneyInPocket, and returns true if it is hot outside and if moneyInPocket is greater than 10.50.
+//Create a function called willBuyDrink that takes a boolean isHotOutside, 
+//and a number moneyInPocket, and returns true if it is hot outside and if moneyInPocket is greater than 10.50.
 console.log("12.");
 function willBuyDrink(isHotOutside,moneyInPocket) {
     if (isHotOutside == true && moneyInPocket > 10.50) {
@@ -97,3 +103,19 @@ willBuyDrink(false, 21); //false
 willBuyDrink(true, 2); //false
 willBuyDrink(false, 2); //false
 
+//13.	Create a function of your own that solves a problem. 
+//In comments, write what the function does and why you created it.
+console.log("13.")
+
+function wantToPlay() {
+    var bored = prompt("Bored?");
+    if (bored == "yes") {
+        alert("Let me suggest a game of Catan!");
+    }  else  {
+        alert("Great!  Have a nice day!");
+        }
+}
+wantToPlay();
+
+//I created this simple function to see if the user is bored and suggests a game if they are. 
+// I created a big (but) simple version of Lemonade Stand before really reading the question.  Since I had games in mind, I decided to run with that!
